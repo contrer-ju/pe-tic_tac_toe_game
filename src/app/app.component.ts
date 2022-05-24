c8271e2dc68dc8ca66ea425b5f1b699d19294137
Juan Contreras <contrerju@gmail.com>
2022-05-24 11:00:58 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'pe-tip_tap_toe_with_angular';
+}
